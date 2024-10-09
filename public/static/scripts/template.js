fe41ca9203e305aad708d24cfaba489bf16cc26f
Sergio S <thelastrkoch@gmail.com>
2024-10-09 22:41:35 +0000

    Ready the Template feature

--- public/static/scripts/template.js
+++ public/static/scripts/template.js
@@ -1,8 +1,67 @@
-// Add event listener for the Execute button
-document.getElementById('textbox').addEventListener('keypress', function() {
-    alert('Execute button clicked!');
-    // Add your execution logic here
+function getTags(inputValue) {
+    regex = /\[(.+?)\]/g
+
+    const tagList = [];
+
+    while ((match = regex.exec(inputValue)) !== null) {
+        tagList.push({ key: match[1], value: "" });
+    }
+    // TODO: add validation currentStatus = "Error trying to get the tags from the input"
+    return tagList
+
+}
+
+function printTags(tagList) {
+    console.log(tagList)
+    queryText = ""
+    for (const tag of tagList) {
+        queryText += tag.key + "=" + tag.value + "\n"
+    }
+    return queryText
+}
+
+
+function reassemblyTags(tagsText) {
+    regex = /(.+?)=(.+?)$/gm
+
+    const tagListReassembled = [];
+
+    while ((match = regex.exec(tagsText)) !== null) {
+        tagListReassembled.push({ key: match[1], value: match[2] });
+    }
+    // TODO: add validation currentStatus = "Error trying to get the tags from the input"
+    return tagListReassembled
+}
+
+
+function replaceTags(inputValue, tagList) {
+    for (const tag of tagList) {
+        inputValue = inputValue.replaceAll(tag.key, tag.value)
+    }
+    inputValue = inputValue.replaceAll("[", "")
+    inputValue = inputValue.replaceAll("]", "")
+    return inputValue
+}
+
+function renderQuery() {
+    outputValue = replaceTags(inputValue, tagList)
+    document.getElementById('txtOutput').value = outputValue
+}
+
+document.getElementById('txtInput').addEventListener('focusout', function () {
+    inputValue = document.getElementById('txtInput').value
+    tagList = getTags(inputValue)
+    tagsText = printTags(tagList)
+    document.getElementById('txtQuery').value = tagsText
 });
 
-// Set the current year in the footer
+
+document.getElementById('txtQuery').addEventListener('focusout', function () {
+    queryText = document.getElementById('txtQuery').value
+    tagListReassembled = reassemblyTags(queryText)
+    outputText = replaceTags(inputValue, tagListReassembled)
+    document.getElementById('txtOutput').value = outputText
+});
+
+
 document.getElementById('currentYear').textContent = new Date().getFullYear();
--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const path = require('path');
 const app = express();
 const PORT = 9000;
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Endpoint to render the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.get('/template', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'template.html'));
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
