3963b263d2b830770726d95135ad832ecdf13ab1
Sergio <thelastrkoch@outlook.com>
2026-01-04 11:24:10 -0600

    Fix all the visual inconsistencies

--- server.js
+++ server.js
@@ -14,8 +14,12 @@ app.get('/template', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'template.html'));
 });
 
-app.get('/editor', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'editor.html'));
+app.get('/text-editor', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'text_editor.html'));
+});
+
+app.get('/code-editor', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'code_editor.html'));
 });
 
 app.listen(PORT, () => {
